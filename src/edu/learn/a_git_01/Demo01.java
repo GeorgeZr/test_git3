f95cc606370ac9cd82d7aeed922ae8868031be13
George <60054849+GeorgeZr@users.noreply.github.com>
2023-10-26 10:10:13 +0800

    Update Demo01.java

--- src/edu/learn/a_git_01/Demo01.java
+++ src/edu/learn/a_git_01/Demo01.java
@@ -4,5 +4,6 @@ public class Demo01 {
     public static void main(String[] args) {
         System.out.println("你好, git");
         System.out.println("你好, git2, 哈哈哈");
+        System.out.println("你好, git2, 哈哈哈, 在 github 上编写的代码");
     }
 }
