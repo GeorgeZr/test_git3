0ccc4933216700abe4c55bcc773eccf2bc715036
qiaoziming <171705210@qq.com>
2023-10-26 10:05:21 +0800

    在 idea 中的 dev1 分支的第一次提交

--- src/edu/learn/a_git_01/Demo01.java
+++ src/edu/learn/a_git_01/Demo01.java
@@ -4,5 +4,6 @@ public class Demo01 {
     public static void main(String[] args) {
         System.out.println("你好, git");
         System.out.println("你好, git2, 哈哈哈");
+        System.out.println("你好, git2, 哈哈哈, 在 dev1 上书写代码的 demo");
     }
 }
