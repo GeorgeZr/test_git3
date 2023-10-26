266782dc374eadf4f8325511d35bc2c8e340c444
qiaoziming <171705210@qq.com>
2023-10-26 09:12:51 +0800

    在 idea 中第二次提交

--- src/edu/learn/a_git_01/Demo01.java
+++ src/edu/learn/a_git_01/Demo01.java
@@ -3,5 +3,6 @@ package edu.learn.a_git_01;
 public class Demo01 {
     public static void main(String[] args) {
         System.out.println("你好, git");
+        System.out.println("你好, git2, 哈哈哈");
     }
 }
